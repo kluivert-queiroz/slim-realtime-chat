c59ab804d2ee27e84609a3d7519eae1fb06524bf
Kluivert Queiroz <kluivert.queiroz@hotmail.com>
2021-06-04 22:57:08 -0300

    log env host

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import socketio from "socket.io-client";
-
+console.log("Connecting to socket host:", process.env.REACT_APP_SOCKET_HOST);
 export const socket = socketio.connect(process.env.REACT_APP_SOCKET_HOST);
 export const SocketContext = React.createContext(socket);
